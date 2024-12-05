a40854813d0dc22f751981700fd25e488feba484
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-05 04:51:19 +0100

    🧼 day-04: clean up

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -1,57 +1,24 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInputMap, count)
+import Advent.Coord    (boundingBox, right, below, left, above)
+import Data.Ix         (range, inRange)
+import Data.Map.Strict (keys, (!))
 
 main =
   do inp <- getInputMap 4
-     print (boundingBox (M.keys inp))
      print (part1 inp)
      print (part2 inp)
 
-part1 m = length
-  [ cs
-  | c <- range b
-  , cs <- ([h,h',v,v',d1,d1',d2,d2'] <*> [c])
-  , inside cs
-  , "XMAS" == map (m M.!) cs
-  ]
+part1 m = count xmas (coords >>= word)
   where
-    Just b = boundingBox (M.keys m)
-    h  c = take 4 (iterate right c)
-    h' c = take 4 (iterate left  c)
-    v  c = take 4 (iterate below c)
-    v' c = take 4 (iterate above c)
-    d1  c = take 4 (iterate (below . right) c)
-    d1' c = take 4 (iterate (above . left ) c)
-    d2  c = take 4 (iterate (above . right) c)
-    d2' c = take 4 (iterate (below . left ) c)
-    inside :: [Coord] -> Bool
-    inside = all (inRange b)
+    Just bounds@(range -> coords) = boundingBox (keys m)
+    look    = [ right, below, below . right, above . right ]
+    word c  = [ cs | d <- look, let cs = take 4 (iterate d c), all (inRange bounds) cs ]
+    xmas cs = [ m ! c | c <- cs ] `elem` [ "XMAS", "SAMX" ]
 
-part2 m = length
-  [ ()
-  | c <- range b
-  , 'A' == m M.! c
-  , let c1 = above (left c); c2 = above (right c); c3 = below (left c); c4 = below (right c)
-  , let cs = [c1,c2,c3,c4]
-  , all inside cs
-  , let xs = map (m M.!) cs
-  , xs `elem` ["MSMS","SSMM","MMSS","SMSM"]
-  ]
+part2 m = count cross (range (cm,cM))
   where
-    Just b = boundingBox (M.keys m)
-    inside = inRange b
+    Just (right . below -> cm,left . above -> cM) = boundingBox (keys m)
+    look    = [ [ left . above, id, right . below ], [ left . below, id, right . above ] ]
+    cross c = all (`elem` [ "MAS", "SAM" ]) [ [ m ! d c | d <- ds ] | ds <- look ]
