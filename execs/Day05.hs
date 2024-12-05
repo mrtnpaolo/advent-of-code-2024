f942dcc6a898437e1a9fd57eb7d10f11105ae1ca
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-05 07:06:30 +0100

    🧼 day-05: clean up

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -1,47 +1,21 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInput)
+import Data.List       (partition, intersect, sortBy)
+import Data.List.Split (splitOn)
 
 main =
-  do inp <- getInput parse 5
-     print (part1 inp)
-     print (part2 inp)
+  do [rs,xss] <- getInput (parse . map clean) 5
+     let (good,bad) = partition (ordered rs) xss
+     print (part1 good)
+     print (part2 rs bad)
   where
-    parse (L.splitOn "\n\n" -> [a,b]) = (ps,ns)
-      where
-        ps = [ let [x,y] = map (read @Int) (L.splitOn "|" l) in (x,y) | l <- lines a ]
-        ns = [ map (read @Int) (L.splitOn "," l) | l <- lines b ]
+    clean '|' = ' '; clean ',' = ' '; clean c = c
+    parse (splitOn "\n\n" -> xs) = map (map (map (read @Int) . words) . lines) xs
+    ordered rs xs = and [ ys == r | r <- rs, let ys = intersect xs r, length ys == 2 ]
 
-ordered (x,y) xs
-  | Just xi <- L.findIndex (x==) xs
-  , Just yi <- L.findIndex (y==) xs
-  = xi < yi
-  | otherwise = True
+part1 xss = sum [ mid xs | xs <- xss ] where mid xs = xs !! (length xs `div` 2)
 
-part1 (ps,nss) = sum [ mid xs | xs <- nss, and [ ordered p xs | p <- ps ] ]
-
-mid xs = xs !! (length xs `div` 2)
-
-part2 (ps,nss) = sum [ mid $ reorder ps xs | xs <- nss, or [ not (ordered p xs) | p <- ps ] ]
-
-reorder ps = L.sortBy f
+part2 rs  = part1 . map (sortBy rules)
   where
-    f x y
-      | (x,y) `elem` ps = LT
-      | (y,x) `elem` ps = GT
-      | otherwise       = compare x y
-
+    rules x y | [x,y] `elem` rs = LT | [y,x] `elem` rs = GT | otherwise = compare x y
