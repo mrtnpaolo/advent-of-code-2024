8e28e1039310b5ad3db6d18ab6ab0fca4a33226a
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-07 06:22:42 +0100

    🧼 day-07: clean up a bit

--- execs/Day07.hs
+++ execs/Day07.hs
@@ -1,27 +1,13 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent (getInputLines)
 
 main =
   do inp <- getInputLines parse 7
      print (part1 inp)
      print (part2 inp)
   where
-    parse = map (read @Int) . words . map \case c | c `elem` ":" -> ' '; c -> c
+    parse = map (read @Int) . words . map \case ':' -> ' '; c -> c
 
 part1 xss = sum [ x | (x:xs) <- xss, any (x ==) (f (reverse xs)) ]
 
