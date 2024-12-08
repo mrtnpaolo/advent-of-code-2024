9fca4dfeeb9a86f96a41d7878fd224b6fddec00a
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-08 13:14:06 +0100

    🧼 day-08: clean up

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,55 +1,20 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Control.Monad
-import Debug.Trace
+import Advent             (getInputArray,pairs,boundingBox)
+import Data.Ix            (inRange)
+import Data.Set           (fromList)
+import Data.Map.Strict    (fromListWith,elems)
+import Data.Array.Unboxed (indices,assocs)
 
 main =
-  do inp <- getInputMap 8
-     print (part1 inp)
-     print (part2 inp)
+  do m <- getInputArray 8
+     let Just (inRange -> inside) = boundingBox (indices m)
+         xs = fromListWith (++) [ (x,[c]) | (c,x) <- assocs m, x /= '.' ]
+     print (part1 inside xs)
+     print (part2 inside xs)
 
-part1 m = length (S.fromList (concat yss))
-  where
-    xs = [ (c,x) | (c,x) <- M.assocs m, x /= '.' ]
-    rs = M.fromListWith (++) [ (x,[c]) | (c,x) <- xs ]
-    Just bs = boundingBox (M.keys m)
-    inside = inRange bs
-    yss =
-      [ extend a b ++ extend b a
-      | (_,cs) <- M.assocs rs
-      , [a,b] <- replicateM 2 cs
-      , a /= b
-      ]
-    extend a b = takeWhile inside [addCoord (scaleCoord 2 d) b]
-      where
-        d = addCoord a (scaleCoord (-1) b)
+part1 inside xs = length $ fromList [ c | cs <- elems xs, (a,b) <- pairs cs, c <- [b+(b-a),a+(a-b)], inside c ]
 
-part2 m = length (S.fromList (concat yss))
+part2 inside xs = length $ fromList [ c | cs <- elems xs, (a,b) <- pairs cs, c <- extend a b ++ extend b a ]
   where
-    xs = [ (c,x) | (c,x) <- M.assocs m, x /= '.' ]
-    rs = M.fromListWith (++) [ (x,[c]) | (c,x) <- xs ]
-    Just bs = boundingBox (M.keys m)
-    inside = inRange bs
-    yss =
-      [ extend a b ++ extend b a
-      | (_,cs) <- M.assocs rs
-      , [a,b] <- replicateM 2 cs
-      , a /= b
-      ]
-    extend a b = tail $ takeWhile inside (iterate (addCoord d) b)
-      where
-        d = addCoord a (scaleCoord (-1) b)
+    extend a b = tail $ takeWhile inside (iterate (d+) b) where d = a-b
