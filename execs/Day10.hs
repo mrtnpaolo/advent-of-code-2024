4a1a386b2e67470f4aa169a5a3ab106ea70bcb74
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-10 07:50:34 +0100

    🧼 day-10: clean up

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -1,21 +1,21 @@
 module Main (main) where
 
 import Advent             (getInputArray,cardinal,count,bfsOn)
-import Data.Array.Unboxed (amap,assocs,(!),(!?))
+import Data.Array.Unboxed (assocs,(!),(!?))
 
 main =
-  do inp <- amap (read @Int . (:[])) <$> getInputArray 10
+  do inp <- getInputArray 10
      print (part1 inp)
      print (part2 inp)
 
-part1 m = sum paths
+part1 m = sum scores
   where
-    starts = [ c | (c,0) <- assocs m ]
-    nexts c = [ d | d <- cardinal c, let x = m ! c, Just y <- [m !? d], x+1 == y ]
-    paths = [ count ((9==) . (m !)) $ bfsOn id nexts [start] | start <- starts ]
+    starts  = [ c | (c,'0') <- assocs m ]
+    nexts c = [ d | d <- cardinal c, Just y <- [m !? d], succ (m ! c) == y ]
+    scores  = [ count (\c -> m ! c == '9') (bfsOn id nexts [start]) | start <- starts ]
 
-part2 m = sum paths
+part2 m = ratings
   where
-    starts = [ c | (c,0) <- assocs m ]
-    nexts cs@(c:_) = [ (d:cs) | d <- cardinal c, let x = m ! c, Just y <- [m !? d], x+1 == y ]
-    paths = [ count (\(c:_) -> m ! c == 9) (bfsOn id nexts [[start]]) | start <- starts ]
+    starts         = [ [c] | (c,'0') <- assocs m ]
+    nexts cs@(c:_) = [ (d:cs) | d <- cardinal c, Just y <- [m !? d], succ (m ! c) == y ]
+    ratings        = count (\(c:_) -> m ! c == '9') (bfsOn id nexts starts)
