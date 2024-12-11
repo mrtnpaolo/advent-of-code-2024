8960933ff659056b6fee0a52a0f9c536ec733fa8
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-11 09:38:16 +0100

    🤌 day-11: simpler shorter faster solution

--- execs/Day11.hs
+++ execs/Day11.hs
@@ -1,16 +1,14 @@
 module Main (main) where
 
-import Advent          (getInput)
-import Data.Foldable   (foldl')
-import Data.List       (group,sort)
-import Data.Map.Strict (fromList,(!),insertWith)
+import Advent      (getInput)
+import Data.IntMap (fromListWith,elems,assocs)
 
 main =
   do inp <- getInput parse 11
      print (part1 inp)
      print (part2 inp)
   where
-    parse = map (read @Int) . words
+    parse xs = fromListWith (+) [ (read @Int x,1) | x <- words xs ]
 
 part1 = blink 25
 
@@ -20,15 +18,5 @@ step 0 = [1]
 step n | xs <- show n, l <- length xs, even l, k <- l `div` 2 = map (read @Int) [take k xs,drop k xs]
        | otherwise = [n * 2024]
 
-blink k xs = go k 0 (fromList xs') [ n | ((n,0),_) <- xs' ]
-  where
-    xs' = [ ((n,0),length ns) | ns@(n:_) <- group (sort xs) ]
-
-go k i a xs
-  | i == k    = sum [ a ! (x,k) | x <- xs ]
-  | otherwise = go k i' a' xs'
-  where
-    i'  = succ i
-    ys  = [ ((n,i'),mul) | x <- xs, let mul = a ! (x,i), n <- step x ]
-    a'  = foldl' (\m (k,v) -> insertWith (+) k v m) a ys
-    xs' = [ x | (x:_) <- group (sort [ x | ((x,_),_) <- ys ]) ]
+blink 0 m = sum (elems m)
+blink i m = blink (pred i) (fromListWith (+) [ (y,n) | (x,n) <- assocs m, y <- step x ])
