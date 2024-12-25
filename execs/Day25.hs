13aa3426733c01069da17f74bdacfe2c0a708baf
Paolo Martini <mrtnpaolo@protonmail.com>
2024-12-25 21:58:27 +0100

    🧼 day-25: clean up

--- execs/Day25.hs
+++ execs/Day25.hs
@@ -1,33 +1,18 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInput,count)
+import Data.List       (partition,transpose)
+import Data.List.Split (splitOn)
 
 main =
-  do inp <- getInput parse 25
+  do inp <- getInput (parse . map lines . splitOn "\n\n") 25
      print (part1 inp)
-     print (part2 inp)
   where
-    parse (map lines . L.splitOn "\n\n" -> xss) = (map (pins . tail) ls,map (pins . init) ks)
+    parse ss = (map (pins . tail) ls,map (pins . init) ks)
       where
-        (ls,ks) = L.partition (all ('#'==) . head) xss
-        pins = map (count ('#'==)) . L.transpose
+        (ls,ks) = partition (all ('#'==) . head) ss
+        pins = map (count ('#'==)) . transpose
 
-part1 (ls,ks) = length [ undefined | l <- ls, k <- ks, fit l k ]
+part1 (ls,ks) = count fit [ (l,k) | l <- ls, k <- ks ]
 
-fit l k = and [ 5-x >= y | x <- l | y <- k ]
-
-part2 = const ()
+fit (l,k) = and [ 5-x >= y | x <- l | y <- k ]
